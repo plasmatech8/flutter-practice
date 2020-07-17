0dc303abdda1624716184577f6a6655ce6c2e4ed
plasmatech8 <com0004@gmail.com>
2020-07-17 16:12:51 +1000

    initialised Ninja App

---
+++ Flutter Tutorial for Beginners/ninja_id/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: NinjaCard(),
+    ));
+
+class NinjaCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ninja ID Card'),
+      ),
+    );
+  }
+}
