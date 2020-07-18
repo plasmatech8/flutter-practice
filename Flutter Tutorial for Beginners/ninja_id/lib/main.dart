6af110218e87d3d924cb37f8f74b0de27f4c0297
plasmatech8 <com0004@gmail.com>
2020-07-18 16:03:18 +1000

    Updated indent

--- Flutter Tutorial for Beginners/ninja_id/lib/main.dart
+++ Flutter Tutorial for Beginners/ninja_id/lib/main.dart
@@ -8,79 +8,78 @@ class NinjaCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.grey[900],
-        appBar: AppBar(
-          backgroundColor: Colors.grey[850],
-          centerTitle: true,
-          elevation: 0, // No drop-shadow
-          title: Text('Ninja ID Card'),
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        backgroundColor: Colors.grey[850],
+        centerTitle: true,
+        elevation: 0, // No drop-shadow
+        title: Text('Ninja ID Card'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0), // Pad content
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start, // Align to left
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/chun-li.jpg'),
+                radius: 100,
+              ),
+            ),
+            Divider(height: 30, color: Colors.grey[600]), // Line w/ 30px pad
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Chun-Li',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2,
+              ),
+            ),
+            SizedBox(height: 30),
+            Text(
+              'CURRENT NINJA LEVEL',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '8',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2,
+              ),
+            ),
+            SizedBox(height: 30),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10),
+                Text(
+                  'chun.li@example.com',
+                  style: TextStyle(
+                      color: Colors.grey[400], fontSize: 18, letterSpacing: 1),
+                )
+              ],
+            )
+          ],
         ),
-        body: Padding(
-          padding: EdgeInsets.fromLTRB(30, 40, 30, 0), // Pad content
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start, // Align to left
-            children: <Widget>[
-              Center(
-                child: CircleAvatar(
-                  backgroundImage: AssetImage('assets/chun-li.jpg'),
-                  radius: 100,
-                ),
-              ),
-              Divider(height: 30, color: Colors.grey[600]), // Line w/ 30px pad
-              Text(
-                'NAME',
-                style: TextStyle(
-                  color: Colors.grey,
-                  letterSpacing: 2,
-                ),
-              ),
-              SizedBox(height: 10),
-              Text(
-                'Chun-Li',
-                style: TextStyle(
-                  color: Colors.amberAccent[200],
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2,
-                ),
-              ),
-              SizedBox(height: 30),
-              Text(
-                'CURRENT NINJA LEVEL',
-                style: TextStyle(
-                  color: Colors.grey,
-                  letterSpacing: 2,
-                ),
-              ),
-              SizedBox(height: 10),
-              Text(
-                '8',
-                style: TextStyle(
-                  color: Colors.amberAccent[200],
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2,
-                ),
-              ),
-              SizedBox(height: 30),
-              Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.email,
-                    color: Colors.grey[400],
-                  ),
-                  SizedBox(width: 10),
-                  Text(
-                    'chun.li@example.com',
-                    style: TextStyle(
-                        color: Colors.grey[400],
-                        fontSize: 18,
-                        letterSpacing: 1),
-                  )
-                ],
-              )
-            ],
-          ),
-        ));
+      ),
+    );
   }
 }
